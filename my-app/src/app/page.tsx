6349ed41f7756349a02c25aca0226d62a287dbb6
luquinhaixx <lucas.moserdasilva@gmail.com>
2024-06-21 21:40:48 -0300

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="w-screen bg-gradient-to-t from-zinc-900 to-zinc-800 flex flex-col">
       <Header />
       <main className="flex p-10 pt-20 h-screen">
-        <section className="flex items-center flex-col relative w-2/5 h-screen bg-white bg-opacity-5 backdrop-blur-md rounded-[40px] top-20 text-white border border-white/10">
+        <section className="flex items-center flex-col relative w-[400px] h-screen bg-white bg-opacity-5 backdrop-blur-md rounded-[40px] top-20 text-white border border-white/10">
           <img src="https://github.com/lucas-g-silva.png"
             alt="Foto de Perfil"
             className="w-64 h-64 rounded-full relative bottom-10 border border-white/10"
@@ -16,7 +16,7 @@ export default function Home() {
             <p>21/11/2008</p>
             <div className="space-y-3 mt-4">
               <p className="flex gap-3"><Mail size={24} color="white"/>lucas.moserdasilva@gmail.com</p>
-              <p className="flex gap-3"><Smartphone size={24} color="white"/>+55 47 98862-2026</p>
+              <a rel='noopener' title='WhatsApp' target="_blank" href="https://wa.me/5547933819181" className="flex gap-3"><Smartphone size={24} color="white"/>+55 47 93381-9181</a>
               <p className="flex gap-3"><MapPin size={24} color="white"/>Rodeio-SC</p>
             </div>
           </div>
